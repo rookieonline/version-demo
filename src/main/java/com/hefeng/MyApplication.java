9a1b3bbdcd0065488132adfbd9659cb5e68d9fec
rookie <675611058@qq.com>
2021-05-28 22:30:18 +0800

    添加启动程序 -贺烽

---
+++ src/main/java/com/hefeng/MyApplication.java
@@ -0,0 +1,16 @@
+package com.hefeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author rookie
+ * @date 2021/05/28 22:26
+ **/
+@SpringBootApplication
+public class MyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+    }
+}
