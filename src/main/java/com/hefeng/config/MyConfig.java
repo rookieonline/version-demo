c15b8732971474f460b15898b4e4a0b3b1524472
rookie <675611058@qq.com>
2021-05-28 22:47:57 +0800

    添加一个过滤器 -贺烽

---
+++ src/main/java/com/hefeng/config/MyConfig.java
@@ -0,0 +1,13 @@
+package com.hefeng.config;
+
+/**
+ * @author rookie
+ * @date 2021/05/28 22:47
+ * 过滤器
+ **/
+public class MyConfig {
+
+    public void filter() {
+        System.out.println("这是过滤器..");
+    }
+}
